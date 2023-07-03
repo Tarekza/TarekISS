bacc91ec8b145355c5c608623568a06b90e43fde
Tarekza <137062487+Tarekza@users.noreply.github.com>
2023-07-03 12:25:44 +0300

    Update and rename clickme.js to clickme1.js

---
+++ clickme1.js
@@ -0,0 +1,12 @@
+<script>
+var redirectButton = document.getElementById("00000000-0000-0000-0000-000000000000_50913de2-2787-b673-3cbe-b622f9e74d9e_Table");
+
+// Define the function to handle the click event
+function redirectToAnotherPage() {
+            // Redirect to another page
+window.location.href = "https://srv-k2five/Designer/Runtime/Form/Form__Zero/";
+  }
+
+        // Attach the onclick event listener to the button
+redirectButton.onclick = redirectToAnotherPage;
+</script>
