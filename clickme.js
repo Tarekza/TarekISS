d1ed74e3423b5dd8f5353dd1c17476d0c3c9e657
Tarekza <137062487+Tarekza@users.noreply.github.com>
2023-07-03 11:20:58 +0300

    Add files via upload

---
+++ clickme.js
@@ -0,0 +1,10 @@
+<script>
+  // Get the clickable element by its ID
+  var clickableElement = document.getElementById("00000000-0000-0000-0000-000000000000_50913de2-2787-b673-3cbe-b622f9e74d9e_Table");
+
+  // Add a click event listener to the element
+  clickableElement.addEventListener("click", function() {
+    // Redirect to a different page
+    window.location.href = "https://srv-k2five/Designer/Runtime/Form/Form__Zero/";
+  });
+</script>
