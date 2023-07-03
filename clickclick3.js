0fdae643a16d3dd5efad747915eba88f8de9f8bb
Tarekza <137062487+Tarekza@users.noreply.github.com>
2023-07-03 13:50:50 +0300

    Update and rename clickclick2.js to clickclick3.js

--- clickclick3.js
+++ clickclick3.js
@@ -2,8 +2,9 @@ const myDiv = document.querySelector([name="Table4_303"]);
 
 alert("hello");
 // add a click event listener to the div
+if(myDiv){
 myDiv.addEventListener('click', function() {
   // specify the action to take when the div is clicked
   window.location.replace("https://srv-k2five/Designer/Runtime/Form/Form__Zero/");
-});
+})};
 
