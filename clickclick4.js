26b29601ad4a1dbe8ccae7b2ba0706524f55f8e8
Tarekza <137062487+Tarekza@users.noreply.github.com>
2023-07-05 13:52:34 +0300

    Update and rename clickclick4.js to clickclick5.js

--- clickclick4.js
+++
@@ -1,3 +0,0 @@
-document.querySelector([name="Table4_303"]).addEventListener('click', function(e) {
-    window.location.href = 'https://srv-k2five:4446/Designer/Runtime/Form/Form__Zero/';
-}, false);
---
+++ clickclick5.js
@@ -0,0 +1,5 @@
+const divElement = document.querySelector('#00000000-0000-0000-0000-000000000000_50913de2-2787-b673-3cbe-b622f9e74d9e_Table'); 
+
+divElement.addEventListener('click', function() {
+  window.location.href = 'https://srv-k2five:4446/Designer/Runtime/Form/Form__Zero/'; 
+});
